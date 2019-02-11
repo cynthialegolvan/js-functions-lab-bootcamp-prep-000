cee539fb30d2f08491c3655db420e5fe1948d1b7
Cynthia LeGolvan <cynthia.nguyen21@gmail.com>
2019-02-11 17:23:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-return `Happy ${holiday}, ${name}!`
-
+{
+  return `Happy ${holiday}, ${name}!`
+}
 function holidayCountdown(days, mother) {
   `It's ${days} days until ${mother}!`
 }
