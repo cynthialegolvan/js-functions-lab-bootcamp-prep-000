8c02266ad1b128a633ac89356fbba0a4b4a951ca
Cynthia LeGolvan <cynthia.nguyen21@gmail.com>
2019-02-11 17:18:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+function sayhappyHolidays() {
+  return "Happy holidays!"
+}
+sayhappyHolidays()
+
+function sayhappyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+sayhappyHolidaysTo()
+
+function sayhappyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+sayhappyHolidaysTo()
+
+function sayholidayCountdown(days, celebrate) {
+  `It's ${days} days until ${celebrate}!`
+}
+sayholidayCountdown()
+
